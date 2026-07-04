fd7e8a3d98e67a35ec801ba1e98e6798e6906381
Aniket Chindhu Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-07-04 20:47:22 +0530

    Time: 197 ms (1.39%), Space: 88.9 MB (98.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,8 +4,8 @@ class Solution {
         int dp[]=new int[n];
         int minp=price[0];
         for(int i=1;i<dp.length;i++){
-            dp[i]=Math.max(dp[i-1],price[i]-minp);
             if(price[i]<minp)minp=price[i];
+            dp[i]=Math.max(dp[i-1],price[i]-minp);
         }
         return dp[n-1];
     }
