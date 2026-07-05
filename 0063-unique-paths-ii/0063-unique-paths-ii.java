9d13ea319ea23886c56ecddb43c796cecd9879ab
Aniket Chindhu Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-07-05 20:37:04 +0530

    Time: 0 ms (100%), Space: 43.6 MB (45.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,23 +3,23 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][]=new int[m][n];
-        for(int i=0;i<dp.length;i++){
+        for(int i=0;i<m;i++){
             Arrays.fill(dp[i],-1);
         }
-        return dfs(0,0,grid,dp);
+        return dfs(0,0,m,n,grid,dp);
     }
-    int dfs(int i,int j,int[][] grid,int[][] dp){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==1){
+    int dfs(int i,int j,int m,int n,int[][] grid,int[][] dp){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==1){
             return 0;
         }
-        if(i==grid.length-1 && j==grid[0].length-1){
+        if(i==m-1 && j==n-1){
             return 1;
         }
         
         if(dp[i][j]!= -1){
             return dp[i][j];
         }
-        dp[i][j]=dfs(i+1,j,grid,dp)+dfs(i,j+1,grid,dp);
+        dp[i][j]=dfs(i+1,j,m,n,grid,dp)+dfs(i,j+1,m,n,grid,dp);
         return dp[i][j];
     }
 }
