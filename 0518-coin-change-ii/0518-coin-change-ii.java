333ea149d62f1ce3ad24a9e0dbe7ca103725334f
Aniket Chindhu Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-07-12 18:46:53 +0530

    Time: 31 ms (12.32%), Space: 56.7 MB (41.21%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp=new int[coins.length+1][amount+1];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return helpher(amount,coins,0,dp);
+    }
+
+    int helpher(int Ramount, int[] coins,int idx,int[][] dp){
+        if(Ramount==0)return 1;
+        if(Ramount<0 || idx==coins.length)return 0;
+        if(dp[idx][Ramount]!=-1){
+            return dp[idx][Ramount];
+        }
+        
+
+        dp[idx][Ramount]=helpher(Ramount-coins[idx],coins,idx,dp)+helpher(Ramount,coins,idx+1,dp);
+        return dp[idx][Ramount];
+
+    }
+}
