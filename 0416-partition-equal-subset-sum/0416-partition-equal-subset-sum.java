c12a2dc04e6d009f87de28d75640a0386edd1e3f
Aniket Chindhu Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-07-13 12:25:26 +0530

    Time: 38 ms (62.92%), Space: 43.6 MB (92.75%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -12,21 +12,29 @@ class Solution {
         // }
         // return helpher(nums,target,0,0,dp);
         int n=nums.length;
-        boolean[][]dp=new boolean[n+1][target+1];
-        for(int i=0;i<n+1;i++){
-            dp[i][0]=true;
-        }
-        for(int i=1;i<=n;i++){
-            for(int sum=1;sum<=target;sum++){
-                if(sum<nums[i-1]){
-                    dp[i][sum]=dp[i-1][sum];
-                }
-                else{
-                    dp[i][sum]=dp[i-1][sum] || dp[i-1][sum-nums[i-1]];
-                }
+        // boolean[][]dp=new boolean[n+1][target+1];
+        // for(int i=0;i<n+1;i++){
+        //     dp[i][0]=true;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     for(int sum=1;sum<=target;sum++){
+        //         if(sum<nums[i-1]){
+        //             dp[i][sum]=dp[i-1][sum];
+        //         }
+        //         else{
+        //             dp[i][sum]=dp[i-1][sum] || dp[i-1][sum-nums[i-1]];
+        //         }
+        //     }
+        // }
+        boolean dp[]=new boolean[target+1];
+        dp[0]=true;
+        for(int num:nums){
+            for(int i=target;i>=num;i--){
+                dp[i]=dp[i]||dp[i-num];
             }
         }
-        return dp[n][target];
+
+        return dp[target];
 
     }
 
