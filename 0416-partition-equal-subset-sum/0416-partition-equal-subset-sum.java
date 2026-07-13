6206cf5c4ca9f4cc793e9fcc54bb44c15cf8857a
Aniket Chindhu Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-07-13 11:58:56 +0530

    Time: 45 ms (58.83%), Space: 47 MB (62.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,22 +6,39 @@ class Solution {
         }
         if(tamount%2!=0)return false;
         int target=tamount/2;
-        int[][]dp=new int[nums.length+1][target+1];
+        // int[][]dp=new int[nums.length+1][target+1];
+        // for(int i=0;i<dp.length;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return helpher(nums,target,0,0,dp);
+
+        boolean[][]dp=new boolean[nums.length+1][target+1];
         for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
+            dp[i][0]=true;
         }
-        return helpher(nums,target,0,0,dp);
+        for(int i=1;i<=nums.length;i++){
+            for(int sum=1;sum<=target;sum++){
+                if(sum<nums[i-1]){
+                    dp[i][sum]=dp[i-1][sum];
+                }
+                else{
+                    dp[i][sum]=dp[i-1][sum] || dp[i-1][sum-nums[i-1]];
+                }
+            }
+        }
+        return dp[nums.length][target];
+
     }
 
-    boolean helpher(int[] nums,int target,int idx,int currsum,int[][] dp){
-        if(currsum==target){
-            return true;
-        }
-        if(idx==nums.length || currsum>target)return false;
-        if(dp[idx][currsum]!=-1)return dp[idx][currsum]==1;
+    // boolean helpher(int[] nums,int target,int idx,int currsum,int[][] dp){
+    //     if(currsum==target){
+    //         return true;
+    //     }
+    //     if(idx==nums.length || currsum>target)return false;
+    //     if(dp[idx][currsum]!=-1)return dp[idx][currsum]==1;
 
-        boolean ans=(helpher(nums,target,idx+1,currsum,dp) || helpher(nums,target,idx+1,currsum+nums[idx],dp));
-        dp[idx][currsum]=ans?1:0;
-        return ans;
-    }
+    //     boolean ans=(helpher(nums,target,idx+1,currsum,dp) || helpher(nums,target,idx+1,currsum+nums[idx],dp));
+    //     dp[idx][currsum]=ans?1:0;
+    //     return ans;
+    // }
 }
