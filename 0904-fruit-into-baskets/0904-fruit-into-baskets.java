553948e4c6f3a9357ead7e869d1ed58b53484679
Aniket Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-08-20 08:13:17 +0530

    Time: 7 ms (96.85%), Space: 62 MB (98.91%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int totalFruit(int[] f) {
+        int uniqueCount=0;
+        int left=0;
+        int ans=0;
+        int n=f.length;
+        int[] freq=new int[n];
+        for(int right=0;right<n;right++){
+            if(freq[f[right]]==0){
+                uniqueCount++;
+                }
+            freq[f[right]]++;
+            while(uniqueCount>2 && left<=right){
+                freq[f[left]]--;
+                if(freq[f[left]]==0)uniqueCount--;
+                left++;
+            }
+            ans=Math.max(ans,right-left+1);
+        }
+        return ans;
+    }
+}
