ec9afc00b856b99df365f545841f1cf96d6b3c2e
Aniket Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-08-23 20:23:21 +0530

    Time: 0 ms (100%), Space: 43 MB (97.59%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,31 +10,47 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int s=1;
-        ListNode temp=head;
-        while(temp.next!=null){
-            temp=temp.next;
-            s++;
+        // int s=1;
+        // ListNode temp=head;
+        // while(temp.next!=null){
+        //     temp=temp.next;
+        //     s++;
+        // }
+        // if(s==2 && n==1){
+        //     head.next=null;
+        //     return head;
+        // }
+        // if(s==2 && n==2){
+        //     head=head.next;
+        //     return head;
+        // }
+        // if(s==1 || s==n){
+        //     head=head.next;
+        //     return head;
+        // }
+        // temp=head;
+        // int i=1;
+        // while(i!=s-n){
+        //     temp=temp.next;
+        //     i++;
+        // }
+        // temp.next=temp.next.next;
+        // return head;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(n!=0){
+            fast=fast.next;
+            n--;
         }
-        if(s==2 && n==1){
-            head.next=null;
-            return head;
-        }
-        if(s==2 && n==2){
+        if(fast==null){
             head=head.next;
             return head;
         }
-        if(s==1 || s==n){
-            head=head.next;
-            return head;
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
         }
-        temp=head;
-        int i=1;
-        while(i!=s-n){
-            temp=temp.next;
-            i++;
-        }
-        temp.next=temp.next.next;
+        slow.next=slow.next.next;
         return head;
     }
 }
