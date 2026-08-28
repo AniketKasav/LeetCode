c9a37347d1008f709d70058817e208714e277b50
Aniket Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-08-28 11:56:03 +0530

    Time: 4 ms (67.32%), Space: 94.5 MB (43.48%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -12,14 +12,44 @@ class Solution {
     ListNode curr;
     public boolean isPalindrome(ListNode head) {
         // use the recursion
-        curr=head;
-        return solve(head);
+        // curr=head;
+        // return solve(head);
+
+        // by make half linked list reverse and then compare the two half
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode rev=reverse(slow.next);
+        slow.next=null;
+        while(rev!=null){
+            if(head.val!=rev.val){
+                return false;
+            }
+            head=head.next;
+            rev=rev.next;
+        }
+        return true;
     }
 
-    boolean solve(ListNode head){
-        if(head==null) return true;
-        boolean ans= solve(head.next) && head.val==curr.val;
-        curr=curr.next;
-        return ans;
+    ListNode reverse (ListNode head){
+        ListNode curr=head;
+        ListNode pre=null;
+        while(curr!=null){
+            ListNode nxt=curr.next;
+            curr.next=pre;
+            pre=curr;
+            curr=nxt;
+        }
+        return pre;
     }
+
+    // boolean solve(ListNode head){
+    //     if(head==null) return true;
+    //     boolean ans= solve(head.next) && head.val==curr.val;
+    //     curr=curr.next;
+    //     return ans;
+    // }
 }
