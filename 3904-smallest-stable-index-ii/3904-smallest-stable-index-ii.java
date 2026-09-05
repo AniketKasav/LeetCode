1bd4428880b6a29b5e5445d15d822e36d08721fc
Aniket Kasav <148405076+AniketKasav@users.noreply.github.com>
2026-09-05 07:48:34 +0530

    Time: 4 ms (88.55%), Space: 133.1 MB (27.86%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -3,17 +3,16 @@ class Solution {
         int len=nums.length;
         if(len==1)return 0;
         int[] minArr=new int[len];
-        int[] maxArr=new int[len];
-        maxArr[0]=nums[0];
         minArr[len-1]=nums[len-1];
-        for(int i=1;i<len;i++){
-            maxArr[i]=(nums[i]>maxArr[i-1])?nums[i]:maxArr[i-1];
-        }
         for(int i=len-2;i>=0;i--){
             minArr[i]=(nums[i]<minArr[i+1])?nums[i]:minArr[i+1];
         }
+        
+        int max=Integer.MIN_VALUE;
+
         for(int i=0;i<len;i++){
-            if(maxArr[i]-minArr[i]<=k)return i;
+            max=Math.max(nums[i],max);
+            if(max-minArr[i]<=k)return i;
         }
         return -1;
     }
